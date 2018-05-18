58cee780c1fba2539be5ddeb1a093f892741d1bb
Eric Anholt <eric@anholt.net>
2018-05-18 14:44:26 +0100

    Add fuzzing for writer::show_expr().
    This seems to be stable.

---
+++ fuzz/fuzz_targets/write_expr.rs
@@ -0,0 +1,26 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate glsl;
+extern crate nom;
+use nom::IResult;
+use std::str::from_utf8;
+
+fuzz_target!(|data: &[u8]| {
+    if from_utf8(data).is_ok() {
+        let expr = glsl::parser::expr(data);
+        match expr {
+            IResult::Done(_, expr) => {
+                let mut output = String::new();
+                glsl::writer::glsl::show_expr(&mut output, &expr);
+                output.push(';');
+                let readback_expr = glsl::parser::expr(output.as_bytes());
+                match readback_expr {
+                    IResult::Done(_, readback_expr) => assert_eq!(expr, readback_expr),
+                    _ => panic!("Failed to re-parse '{}'",
+                                output),
+                }
+            }
+            _ => {}
+        }
+    }
+});
