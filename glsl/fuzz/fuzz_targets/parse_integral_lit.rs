298bf5446b487705c6b77b2c416f215070791eb9
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2019-11-12 22:49:59 +0100

    Cleanup fuzzers.

--- glsl/fuzz/fuzz_targets/parse_integral_lit.rs
+++ glsl/fuzz/fuzz_targets/parse_integral_lit.rs
@@ -1,7 +1,8 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 extern crate glsl;
 
 fuzz_target!(|data: &[u8]| {
-    glsl::parsers::integral_lit(data);
+  glsl::parsers::integral_lit(data);
 });
--- glsl/fuzz/fuzz_targets/transpile_expr.rs
+++ glsl/fuzz/fuzz_targets/transpile_expr.rs
@@ -1,26 +1,26 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 extern crate glsl;
 extern crate nom;
 use nom::IResult;
 use std::str::from_utf8;
 
 fuzz_target!(|data: &[u8]| {
-    if from_utf8(data).is_ok() {
-        let expr = glsl::parsers::expr(data);
-        match expr {
-            IResult::Done(_, expr) => {
-                let mut output = String::new();
-                glsl::transpiler::glsl::show_expr(&mut output, &expr);
-                output.push(';');
-                let readback_expr = glsl::parsers::expr(output.as_bytes());
-                match readback_expr {
-                    IResult::Done(_, readback_expr) => assert_eq!(expr, readback_expr),
-                    _ => panic!("Failed to re-parse '{}'",
-                                output),
-                }
-            }
-            _ => {}
+  if from_utf8(data).is_ok() {
+    let expr = glsl::parsers::expr(data);
+    match expr {
+      IResult::Done(_, expr) => {
+        let mut output = String::new();
+        glsl::transpiler::glsl::show_expr(&mut output, &expr);
+        output.push(';');
+        let readback_expr = glsl::parsers::expr(output.as_bytes());
+        match readback_expr {
+          IResult::Done(_, readback_expr) => assert_eq!(expr, readback_expr),
+          _ => panic!("Failed to re-parse '{}'", output),
         }
+      }
+      _ => {}
     }
+  }
 });
