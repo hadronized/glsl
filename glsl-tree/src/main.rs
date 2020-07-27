1a2b4cdf58a50b7a8a6187bd5eadf6eb01fcf2a6
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2020-07-27 14:02:46 +0200

    [glsl-tree] Add glsl-tree.

---
+++ glsl-tree/src/main.rs
@@ -0,0 +1,24 @@
+use glsl::parser::Parse as _;
+use glsl::syntax::ShaderStage;
+use std::io;
+use std::process::exit;
+
+fn main() {
+  let mut content = String::new();
+
+  match io::stdin().read_line(&mut content) {
+    Ok(_) => match ShaderStage::parse(content) {
+      Ok(ast) => println!("{:#?}", ast),
+
+      Err(err) => {
+        eprintln!("cannot parse GLSL:\n{}", err);
+        exit(1);
+      }
+    },
+
+    Err(err) => {
+      eprintln!("cannot read from stdin: {}", err);
+      exit(2);
+    }
+  }
+}
