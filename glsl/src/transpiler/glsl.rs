7abee688691ab3408de4e2f3fe5b70387518625f
Paul Fulham <paul.fulham0@gmail.com>
2021-08-09 14:14:33 -0700

    Add missing do-while and for-init expr semicolons

--- glsl/src/transpiler/glsl.rs
+++ glsl/src/transpiler/glsl.rs
@@ -1438,7 +1438,7 @@ where
       show_statement(f, body);
       let _ = f.write_str(" while (");
       show_expr(f, cond);
-      let _ = f.write_str(")\n");
+      let _ = f.write_str(");\n");
     }
     syntax::IterationStatement::For(ref init, ref rest, ref body) => {
       let _ = f.write_str("for (");
@@ -1475,6 +1475,7 @@ where
       if let Some(ref e) = *expr {
         show_expr(f, e);
       }
+      let _ = f.write_str("; ");
     }
     syntax::ForInitStatement::Declaration(ref d) => show_declaration(f, d),
   }
